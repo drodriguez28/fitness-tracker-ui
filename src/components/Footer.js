8531c6116d4a767c9a1a17cc00e84cd85bf9562f
Kevin <kevin.rodriguez678@gmail.com>
2022-10-11 16:23:44 -0400

    add footer to home page

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function footer(){
+  return(
+    <div class="flex flex-col min-h-screen
+">
+      <footer class="mt-auto">
+        <p>Created by:
+          <a href="https://www.linkedin.com/in/deivid-rodriguez-635875186/"> Deivid Rodriguez </a>
+          <a href="https://www.linkedin.com/in/kevin-a-rodriguez/">Kevin Rodriguez</a>
+        </p>
+      </footer>
+    </div>
+  )
+}
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import HomeCard from "../components/HomeCard";
+import Footer from "../components/Footer";
 
 export default function HomePage() {
   return (
     <div>
       <h1 class="text-3xl font-bold ">Fitness Tracker</h1>
       <HomeCard/>
+      <Footer/>
     </div>
   );
 }
