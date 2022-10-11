45a00586f3f43dc87dc93316a40b989ad9fa9412
Deivid Rodriguez <rodriguezdeivid17@gmail.com>
2022-10-11 16:46:50 -0400

    added correct format

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,20 @@
 import React from "react";
 
-export default function Footer(){
-  return(
-    <div class="flex flex-col min-h-screen
-">
+export default function Footer() {
+  return (
+    <div class="flex flex-col min-h-screen">
       <footer class="mt-auto">
-        <p>Created by:
-          <a href="https://www.linkedin.com/in/deivid-rodriguez-635875186/"> Deivid Rodriguez </a>
-          <a href="https://www.linkedin.com/in/kevin-a-rodriguez/">Kevin Rodriguez</a>
+        <p>
+          Created by:
+          <a href="https://www.linkedin.com/in/deivid-rodriguez-635875186/">
+            {" "}
+            Deivid Rodriguez{" "}
+          </a>
+          <a href="https://www.linkedin.com/in/kevin-a-rodriguez/">
+            Kevin Rodriguez
+          </a>
         </p>
       </footer>
     </div>
-  )
+  );
 }
