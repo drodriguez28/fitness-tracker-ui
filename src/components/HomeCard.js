707b2c7e1be266a0c129d1c2f80cae02521bb155
Deivid Rodriguez <rodriguezdeivid17@gmail.com>
2022-10-12 15:33:31 -0400

    working on fixing icon

--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -5,7 +5,7 @@ export default function HomeCard() {
     <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 md:gap-8 pt-5 mx-14">
       <div>
         <a
-          href="#"
+          href="/workouts"
           class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
         >
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -19,7 +19,7 @@ export default function HomeCard() {
 
       <div>
         <a
-          href="#"
+          href="/routes"
           class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
         >
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -33,7 +33,7 @@ export default function HomeCard() {
 
       <div>
         <a
-          href="#"
+          href="/community"
           class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
         >
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,13 @@
 import React from "react";
 import HomeCard from "../components/HomeCard";
 import Footer from "../components/Footer";
+import NavMenu from "../components/NavMenu";
 
 export default function HomePage() {
   return (
     <div>
-      <h1 class="text-3xl font-bold ">Fitness Tracker</h1>
-      <HomeCard/>
+      <NavMenu/>
+      {/* <HomeCard/> */}
       <Footer/>
     </div>
   );
