f3d3855c6e4ffb8612ce8b4042316a0818f1c670
Deivid Rodriguez <rodriguezdeivid17@gmail.com>
2022-11-03 15:03:46 -0400

    added modifications

--- src/components/Cmtytable.js
+++ src/components/Cmtytable.js
@@ -3,8 +3,7 @@ import React from 'react'
 const Cmtytable = () => {
   return (
     <div>
-      
-      <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
+      <div class="overflow-x-auto shadow-md sm:rounded-lg">
           <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
               <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                   <tr>
--- src/pages/WorkoutPage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function WorkoutPage() {
-  return (
-    <div>
-      <h1>Workout Exercises</h1>
-    </div>
-  );
-}
